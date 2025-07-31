09f70ad58b4eb3aaa0e912aaae2263f047635774
spideycodes0utk <utkarsh.patil_609@walchandsangli.ac.in>
2025-07-31 21:47:19 +0530

    Add files via upload

---
+++ 246109045_utk_spideycodes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//SPIDEY CODES...
+
+int main(){
+    int a,b;
+    cout<<"\nEnter two numbers:";
+    cin>>a>>b;
+    cout<<"\nSum of two numbers is:"<<a+b;
+    return 0;
+}
