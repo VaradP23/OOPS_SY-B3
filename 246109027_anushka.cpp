774571ca9edd89250ab23128e43ad6d7477346fe
ANUSHKAo13 <anushka.patil_536@walchandsangli.ac.in>
2025-07-31 19:48:26 +0530

    Add files via upload

---
+++ 246109027_anushka.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"\nEnter two numbers:";
+    cin>>a>>b;
+    cout<<"\nSum of two numbers is:"<<a+b;
+    return 0;
+}
